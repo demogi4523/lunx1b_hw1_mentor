5064ba9dc9f285ea33ec05dab7d3da16783ba6e5
demogi4523 <demogi4523@yandex.ru>
2023-03-02 23:32:12 +0800

    (feat): add method 'add' in module for working with 'Big Numbers'

---
+++ second/demo.js
@@ -0,0 +1,21 @@
+import { BiggyNumber } from "./index.js";
+
+// const num1 = new BiggyNumber("12345123451234512345");
+// const num2 = new BiggyNumber("54321543215432154321");
+const num1 = new BiggyNumber("7236");
+const num2 = new BiggyNumber("24345");
+// const num3 = new BiggyNumber();
+// const num4 = new BiggyNumber();
+
+// console.log(num1.toString());
+console.log(num1);
+// console.log(num1._get_length());
+// console.log(num2.toString());
+console.log(num2);
+// console.log(num2._get_length());
+console.log("(num1 + num2): ");
+console.log(num1.add(num2));
+// console.log(num1.add(num2).toString());
+// console.log("(num1 * num2): " + num1.multiply(num2));
+// console.log("(num1 * num2 / num3): " + num1.multiply(num2).divide(num3));
+// console.log("(num1 * num2 / num3 - num4): " + num1.multiply(num2).divide(num3).subtract(num4));
--- second/index.js
+++ second/index.js
@@ -1,16 +1,78 @@
 // 2.Написать модуль, который способен выполнять операции с числами любой длины.
 // Методы для сложения, умножения, вычитания и деления.
 export class BiggyNumber{
-    constructor() {
-
-    }
-
-    add(otherBiggyNumber) {
+    // val is string
+    constructor(val) {
+        this._input = val;
+        this.val = this._init(this._input);
+        // this.fixed_length = 15; // Number.MAX_SAFE_INTEGER.length - 1
+        this.fixed_length = 2;
         
     }
 
-    subtract(otherBiggyNumber) {
+    // val is string
+    _init(val) {
+        const res = [];
+        const reverse_val = val.split('').reverse().join('');
+        // const fixed_length = 15;
+        const fixed_length = 2;
+        let val_length = val.length;
+        let start = 0;
 
+        while (start < val_length) {
+            const s = reverse_val.slice(start, start + fixed_length);
+            const rev_s = s.split('').reverse().join('');
+            res.push(rev_s);
+            start += fixed_length;
+        }
+        return res.reverse();
+    }
+
+    _get_length() {
+        return Math.ceil(this._input.length / this.fixed_length);
+    }
+
+    add(otherBiggyNumber) {
+        if (this._get_length() > otherBiggyNumber._get_length()) {
+            return otherBiggyNumber.add(this);
+        }
+
+        const res = [];
+
+        let reminder = 0;
+        let currentGroupIndex = 1;
+        const len = this._get_length();
+        const otherLen = otherBiggyNumber._get_length();
+        const coMultiplier = Math.pow(10, this.fixed_length);
+
+        while (len >= currentGroupIndex) {
+            const currentGroup = this.val[len - currentGroupIndex];
+            const currentOtherGroup = otherBiggyNumber.val[otherLen - currentGroupIndex];
+            const groupSumInt = parseInt(currentGroup) + parseInt(currentOtherGroup) + reminder;
+            
+            let currentResGroup = groupSumInt.toString();
+            if (currentResGroup > coMultiplier) {
+                reminder = 1;
+                currentResGroup -= coMultiplier;
+            }
+            res.push(currentResGroup.toString());
+
+            currentGroupIndex += 1;
+        }
+
+        for (let index = currentGroupIndex; index <= otherLen; index++) {
+            const group = otherBiggyNumber.val[otherLen - index];
+            res.push((parseInt(group) + reminder).toString());
+            reminder = 0;
+        }
+        res.reverse();
+        const q = res.join('');
+        // console.log(q);
+        return new BiggyNumber(q);
+    }
+
+    subtract(otherBiggyNumber) {
+        
     }
     
     multiply(otherBiggyNumber) {
@@ -25,5 +87,8 @@ export class BiggyNumber{
 
     }
 
-    // toString() {}
+    toString() {
+        // console.log(this.val);
+        return this.val.join('');
+    }
 }
